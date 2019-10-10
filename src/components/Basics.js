8a453b20c024bebac1b2a32ea84f661c61b45593
John Blackwell <john.blackwell@rtslabs.com>
2019-10-09 18:51:28 -0700

    add some color picking to the basic example

--- src/components/Basics.js
+++ src/components/Basics.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
 import { useSpring } from 'react-spring';
+import { Form, Field } from 'jdb-components';
 import Highlight from 'react-highlight';
 import * as S from './styles';
 const Basics = () => {
   const [on, toggle] = useState(false);
+  const [firstColor, setFirstColor] = useState('teal');
+  const [secondColor, setSecondColor] = useState('white');
+  const firstOptions = [
+    { id: 0, value: 'skyblue' },
+    { id: 1, value: 'tomato' },
+    { id: 2, value: 'khaki' },
+    { id: 3, value: 'white' },
+    { id: 4, value: 'magenta' },
+    { id: 5, value: 'olive' },
+  ];
+  const secondOptions = [
+    { id: 0, value: 'orangered' },
+    { id: 1, value: 'maroon' },
+    { id: 2, value: 'seagreen' },
+    { id: 3, value: 'crimson' },
+    { id: 4, value: 'black' },
+    { id: 5, value: 'lime' },
+  ];
   const spring = useSpring({
-    background: on ? 'teal' : '#fff',
-    height: on ?'50vh' : '5rem',
+    background: on ? firstColor : secondColor,
+    height: on ? '50vh' : '5rem',
     width: on ? '50vw' : '5rem',
     transform: on ? 'rotate(90deg)' : 'rotate(0deg)',
   });
@@ -14,9 +33,36 @@ const Basics = () => {
     <S.Page color="cornflowerblue">
       <S.ColDiv align="center">
         <S.RotatingBox style={spring} />
-        <S.ToggleButton style={{marginTop: '5rem'}} onClick={() => toggle(!on)}>
+        <S.ToggleButton
+          style={{ marginTop: '5rem' }}
+          onClick={() => toggle(!on)}
+        >
           Toggle Animation
         </S.ToggleButton>
+        <S.ColDiv width="40%">
+          <Form
+            onSubmit={values => {
+              const { first_color, second_color } = values;
+              setFirstColor(first_color);
+              setSecondColor(second_color);
+            }}
+          >
+            <S.RowDiv justify='space-between'>
+              <Field
+                type="select"
+                options={firstOptions}
+                required
+                fieldName="first_color"
+              />
+              <Field
+                type="select"
+                options={secondOptions}
+                required
+                fieldName="second_color"
+              />
+            </S.RowDiv>
+          </Form>
+        </S.ColDiv>
         <Highlight language="javascript">
           {`const Basics = () => {
   const [on, toggle] = useState(false);
